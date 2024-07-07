bdfce86096c280f3790eb77cc0f2511ffd8c4f13
narinn-star <skfls2618@naver.com>
2024-07-08 02:20:05 +0900

    Leet_[141]_24.07.08

---
+++ leetcode/141_linked_list_cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (head === null) return false;
+
+  let first = head;
+  let second = head.next;
+
+  while (first !== second) {
+    if (second === null || second.next === null) return false;
+    first = first.next;
+    second = second.next.next;
+  }
+
+  return true;
+};
