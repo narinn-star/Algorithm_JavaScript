420299b3dd78bbb708e3e1b9ce0334a2276995a3
narinn-star <skfls2618@naver.com>
2024-07-09 00:57:27 +0900

    Goorm_[꽃 선물하기]_24.07.09

---
+++ goorm/꽃 선물하기.js
@@ -0,0 +1,29 @@
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+  let rl = readline.createInterface({ input: process.stdin });
+
+  let N = null;
+  let arr = [];
+
+  for await (const line of rl) {
+    if (!N) N = +line;
+    else {
+      arr.push(line.split(' ').map(Number));
+      if (arr.length === N) rl.close();
+    }
+  }
+
+  solution(arr);
+  process.exit();
+})();
+
+const solution = (arr) => {
+  arr.forEach((v) => {
+    let [a, b] = v;
+    if (a < b) console.log('Sunflower');
+    else if (a > b) console.log('Rose');
+    else console.log('Tulip');
+  });
+};
