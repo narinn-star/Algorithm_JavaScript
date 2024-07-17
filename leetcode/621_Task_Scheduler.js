1a7bf4427163fcc9dd501e687086c2f9cff2aa45
narinn-star <skfls2618@naver.com>
2024-07-18 01:05:18 +0900

    Leet_[621]_unsolved

---
+++ leetcode/621_Task_Scheduler.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+const leastInterval = function (tasks, n) {
+  //   const tasksMap = new Map();
+  //   for (let i = 0; i < tasks.length; i++) {
+  //     const t = tasksMap.get(tasks[i]);
+  //     if (t) tasksMap.set(tasks[i], t + 1);
+  //     else tasksMap.set(tasks[i], 1);
+  //   }
+};
