87de342157673faaaab5fa0848e841d6a0a3833a
narinn-star <skfls2618@naver.com>
2024-07-22 01:18:18 +0900

    Goorm_[두부 자르기]_24.07.22

---
+++ goorm/두부 자르기.js
@@ -0,0 +1,20 @@
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+  let rl = readline.createInterface({ input: process.stdin });
+
+  let N = null;
+
+  for await (const line of rl) {
+    N = +line;
+    rl.close();
+  }
+
+  solution(N);
+  process.exit();
+})();
+
+const solution = (N) => {
+  console.log(N / 2 + 1);
+};
