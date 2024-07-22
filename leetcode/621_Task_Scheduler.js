57c3b7cc1c2d454890799c31dd45c7d68152b6dc
narinn-star <skfls2618@naver.com>
2024-07-23 03:13:24 +0900

    Leet_[621]_24.07.19

--- leetcode/621_Task_Scheduler.js
+++ leetcode/621_Task_Scheduler.js
@@ -10,4 +10,22 @@ const leastInterval = function (tasks, n) {
   //     if (t) tasksMap.set(tasks[i], t + 1);
   //     else tasksMap.set(tasks[i], 1);
   //   }
+
+  let count = Array(26).fill(0);
+
+  for (let t of tasks) {
+    count[t.charCodeAt(0) - 'A'.charCodeAt(0)]++; // 테스크 개수 세기
+  }
+
+  count.sort((a, b) => b - a); //
+  let most = count[0];
+
+  let idle = (most - 1) * n;
+
+  for (let i = 1; i < 26; i++) {
+    idle -= Math.min(most - 1, count[i]);
+  }
+
+  if (idle < 0) return tasks.length;
+  else return tasks.length + idle;
 };
