8abbbfb3e35c67eb5563a55cb39b53ae30f3c25f
narinn-star <skfls2618@naver.com>
2024-08-18 20:53:12 +0900

    Programmers_[핸드폰 번호 가리기]_24.08.18

---
+++ programmers/핸드폰 번호 가리기.js
@@ -0,0 +1,13 @@
+function solution(phone_number) {
+  var answer = '';
+  for (let i = 0; i < phone_number.length - 4; i++) {
+    answer += '*';
+  }
+  for (let i = phone_number.length - 4; i < phone_number.length; i++) {
+    answer += phone_number[i];
+  }
+  return answer;
+}
+
+console.log(solution('01033334444'));
+console.log(solution('027778888'));
