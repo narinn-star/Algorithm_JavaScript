39243a52f369bea034024859a7fe2d495a49df8f
narinn-star <skfls2618@naver.com>
2024-08-22 17:42:06 +0900

    Leet_[189]_24.08.22

--- leetcode/189_Rotate_Array.js
+++ leetcode/189_Rotate_Array.js
@@ -5,17 +5,28 @@
  */
 
 /**
- * Topics :
+ * Topics : Array, Math, Two Pointers
  *
  * Problem
- *
+ * k만큼 rotate시킨 배열 리턴하기
+ * 단, 주어진 nums를 활용해야함..
  *
  * Explanation
- * 1.
+ *
  */
 const rotate = (nums, k) => {
-  k;
-  return nums;
+  let len = nums.length;
+
+  k = k % len;
+
+  if (k == len || len == 1) return;
+
+  let arr = nums.slice(len - k);
+  arr.push(...nums.slice(0, len - k));
+
+  for (let i = 0; i < len; i++) {
+    nums[i] = arr[i];
+  }
 };
 
 console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
