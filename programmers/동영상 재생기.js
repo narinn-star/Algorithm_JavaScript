b99f34aecba65db36a63f2a3fff51964317e91c4
narinn-star <skfls2618@naver.com>
2024-10-10 14:52:36 +0900

    Programmers_[동영상 재생기]_24.10.10

--- programmers/동영상 재생기.js
+++ programmers/동영상 재생기.js
@@ -1,10 +1,24 @@
 /**
+ * Topics: PCCP 기출문제
+ *
+ * mm:ss형태의 문자열을 다루는거라, 실제 개발환경에서도 많이 쓰기도 해서 더 재밌었던 문제였다.
+ *
+ * 기본적으로 시간을 계산할 땐 mm:ss 형태를 seconds로 모두 바꾸고 계산한 후에 다시 mm:ss형태로 바꿔주는게 쉽다.
+ * 보편적인 것 같기도 하고..
+ *
+ * 그래서 timeToSeconds, secondsToTime 함수를 각각 만들어뒀고, prev, next 함수도 따로 작성했다.
+ * 중요한건 10초 전후로 넘겼을 때 오프닝에 걸리는가를 확인해야해서 중간중간 냅다 다 넣어버림.. 이거 괜찮은 거 맞나 모르겠는데 최선임..
+ *
+ * 확실히 그냥 알고리즘보다 구현이 더 재미있기는 한데 더 복잡해지면 배로 힘들어짐 ㅠㅠ
+ */
+
+/**
  *
  * @param {String} video_len
  * @param {String} pos
  * @param {String} op_start
- * @param {*} op_end
- * @param {*} commands
+ * @param {String} op_end
+ * @param {String[]} commands
  * @returns
  */
 function solution(video_len, pos, op_start, op_end, commands) {
