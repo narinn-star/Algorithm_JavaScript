17adff7065286ea1d38e97cceab20a544393cf8f
narinn-star <skfls2618@naver.com>
2024-11-11 17:55:22 +0900

    Programmers_[콜라 문제]_24.11.11

---
+++ programmers/콜라 문제.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} n
+ * @returns {number}
+ */
+function solution(a, b, n) {
+  let sum = 0;
+  let empty = n;
+
+  while (empty >= a) {
+    const give = a * Math.floor(empty / a);
+    empty = empty - give + (give / a) * b;
+    sum += (give / a) * b;
+  }
+
+  return sum;
+}
