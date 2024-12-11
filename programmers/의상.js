0ba28694717fa060ce8c4c33e69e40ccf793a4bb
narinn-star <skfls2618@naver.com>
2024-12-11 23:59:49 +0900

    Programmers_[의상]_24.12.11

---
+++ programmers/의상.js
@@ -0,0 +1,28 @@
+/**
+ * Topic: 해시
+ */
+
+/**
+ * @param {String[][]} clothes
+ * @returns {Number[]}
+ */
+
+function solution(clothes) {
+  const map = {};
+  let ans = 1;
+
+  clothes.forEach((arr) => {
+    const [type, name] = arr;
+    if (map.hasOwnProperty(name)) {
+      map[name]++;
+    } else {
+      map[name] = 1;
+    }
+  });
+
+  for (const key in map) {
+    ans *= map[key] + 1;
+  }
+
+  return ans - 1;
+}
