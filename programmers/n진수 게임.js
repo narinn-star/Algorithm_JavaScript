90366a5b9af7aaf535ed5bfd5e26125e0a2470de
narinn-star <skfls2618@naver.com>
2025-01-07 17:19:14 +0900

    Programmers_[n진수 게임]_25.01.07

---
+++ programmers/n진수 게임.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number} t
+ * @param {number} m
+ * @param {number} p
+ * @returns {string}
+ */
+function solution(n, t, m, p) {
+  let str = '';
+
+  // 숫자 모두 구하기
+  for (let i = 0; i < m * t; i++) {
+    str += i.toString(n).toUpperCase();
+  }
+
+  let answer = '';
+  let num = 0;
+
+  while (answer.length < t) {
+    const s = str.substring(num, num + m);
+    answer += s[p - 1];
+
+    num += m;
+  }
+
+  return answer;
+}
