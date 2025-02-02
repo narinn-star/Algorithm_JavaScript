9af58f878b6dafa0382555b8ba2fa58edeef8a37
narinn-star <skfls2618@naver.com>
2025-02-02 23:59:38 +0900

    Programmers_[문자열안에 문자열]_25.02.02

---
+++ programmers/문자열안에 문자열.js
@@ -0,0 +1,14 @@
+/**
+ * Topic: 코딩테스트 입문
+ *
+ * includes 함수
+ */
+/**
+ *
+ * @param {string} str1
+ * @param {string} str2
+ * @returns number
+ */
+function solution(str1, str2) {
+  return str1.includes(str2) === true ? 1 : 2;
+}
