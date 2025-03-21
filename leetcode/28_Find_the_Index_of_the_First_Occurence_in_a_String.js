9337dc9c584af5538d73729860c4a15a25b88936
narinn-star <skfls2618@naver.com>
2025-03-21 16:19:38 +0900

    Leet_[28]_25.03.21

---
+++ leetcode/28_Find_the_Index_of_the_First_Occurence_in_a_String.js
@@ -0,0 +1,11 @@
+/**
+ * Topics: Two Pointers, String, String Matching
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
