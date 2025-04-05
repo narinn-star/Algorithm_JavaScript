04fa1d53d4b80d7f757c46dda65f116c0c02db9b
narinn-star <skfls2618@naver.com>
2025-04-05 16:21:43 +0900

    Leet_[226]_25.04.05

---
+++ leetcode/226_Invert_Binary_Tree.js
@@ -0,0 +1,27 @@
+/**
+ * Topics: Tree, Depth-First Search, Breadth-First Search, Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (root === null) return null;
+
+  const tmp = root.left;
+  root.left = root.right;
+  root.right = tmp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
