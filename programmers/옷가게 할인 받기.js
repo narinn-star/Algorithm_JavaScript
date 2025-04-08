5093919f9489cd9a1f721e4e20afe1681ebb9611
narinn-star <skfls2618@naver.com>
2025-04-09 00:18:36 +0900

    Programmers_[옷가게 할인 받기]_25.04.09

---
+++ programmers/옷가게 할인 받기.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {number} price
+ * @returns {number}
+ */
+function solution(price) {
+  if (price >= 500000) {
+    return Math.floor(price - price * 0.2);
+  } else if (price >= 300000) {
+    return Math.floor(price - price * 0.1);
+  } else if (price >= 100000) {
+    return Math.floor(price - price * 0.05);
+  }
+  return price;
+}
