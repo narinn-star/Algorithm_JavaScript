e928f20f9a09c55c3ab2010136efc819fd6cb740
narinn-star <skfls2618@naver.com>
2025-04-25 23:34:20 +0900

    Leet_[168]_25.04.25

---
+++ leetcode/168_Excel_Sheet_Column_Title.js
@@ -0,0 +1,17 @@
+/**
+ * Topics: Math, String
+ */
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function (columnNumber) {
+  let res = '';
+  while (columnNumber > 0) {
+    columnNumber--;
+    res = String.fromCharCode((columnNumber % 26) + 65) + res;
+    columnNumber = Math.floor(columnNumber / 26);
+  }
+
+  return res;
+};
